7cc1897c065f10f8af069b5c08b7abdcc49fe54f
belicfr <96206878+belicfr@users.noreply.github.com>
2024-12-04 04:42:24 -0500

    refactor: reset DB::connect() as private

--- src/database/DB.php
+++ src/database/DB.php
@@ -18,7 +18,7 @@ class DB
      *
      * @return bool If connection attempt is successful
      */
-    public static function connect(): bool
+    private static function connect(): bool
     {
         $db = self::db();
         $dsn = "{$db['dbms']}:host={$db['host']};port={$db['port']}"
