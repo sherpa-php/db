cdb8745b5f79e603e039c2537b24169123df0f06
belicfr <96206878+belicfr@users.noreply.github.com>
2024-12-04 04:58:23 -0500

    feat: implement where and orWhere methods

--- src/database/DatabaseQuery.php
+++ src/database/DatabaseQuery.php
@@ -2,6 +2,8 @@
 
 namespace Sherpa\Db\database;
 
+use Sherpa\Db\database\enums\Operator;
+
 class DatabaseQuery
 {
     private string $table;
@@ -20,8 +22,58 @@ class DatabaseQuery
         $this->table = $table;
     }
 
-    public function select(array|string $columns = "*")
+    /**
+     * Defines columns to select.
+     * <p>
+     *     If none column is provided,
+     *     query will select all columns ('*').
+     * </p>
+     *
+     * @param array|string $columns
+     * @return DatabaseQuery
+     */
+    public function select(array|string $columns = "*"): self
     {
-        // TODO
+        $this->columns = is_string($columns)
+            ? [$columns]
+            : $columns;
+
+        return $this;
+    }
+
+    /**
+     * Adds a condition using AND operator by default.
+     *
+     * @param string $column
+     * @param mixed $operatorOrValue Comparison operator or value
+     *                               (shortcut using '=')
+     * @param mixed $value Value if shortcut is not used
+     * @param Operator $operator Condition operator, used on conditions joining,
+     *                           AND operator is used by default
+     * @return $this
+     */
+    public function where(string $column,
+                          mixed $operatorOrValue,
+                          mixed $value = null,
+                          Operator $operator = Operator::AND): self
+    {
+        $comparisonOperator = $value === null
+            ? '='
+            : $operatorOrValue;
+
+        $value = $value ?? $operatorOrValue;
+
+        $this->conditions[] = new Condition(
+            $column, $comparisonOperator, $value, $operator);
+
+        return $this;
+    }
+
+    public function orWhere(string $column,
+                            mixed $operatorOrValue,
+                            mixed $value = null): self
+    {
+        return self::where(
+            $column, $operatorOrValue, $value, Operator::OR);
     }
 }
