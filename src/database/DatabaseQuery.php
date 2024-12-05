ed9fc9253bdcc36f7294422993b1bd8cd470e260
belicfr <96206878+belicfr@users.noreply.github.com>
2024-12-05 03:03:46 -0500

    feat: implement DB query getter

--- src/database/DatabaseQuery.php
+++ src/database/DatabaseQuery.php
@@ -5,4 +5,10 @@ namespace Sherpa\Db\database;
 class DatabaseQuery extends Query
 {
     public function get(array $columns = ["*"]): array
+    {
+        $sql = $this->sql();
+        $parameters = $this->parameters;
+
+        return DB::run($sql, $parameters);
+    }
 }
