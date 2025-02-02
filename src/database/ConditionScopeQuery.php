b2850c0e91162741dcfe37ca562e5ce9a2a936ff
belicfr <96206878+belicfr@users.noreply.github.com>
2025-02-02 17:49:29 -0500

    fix: initialize ConditionScopeQuery with empty array

--- src/database/ConditionScopeQuery.php
+++ src/database/ConditionScopeQuery.php
@@ -6,7 +6,7 @@ use Sherpa\Db\database\Query;
 
 class ConditionScopeQuery extends Query
 {
-    public private(set) array $conditions;
+    public private(set) array $conditions = [];
 
     public function __construct(string $table)
     {
