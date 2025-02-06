d048d827bfa2266ab47c057f2f11e3da9c27228b
belicfr <96206878+belicfr@users.noreply.github.com>
2025-02-06 13:43:50 -0500

    refactor: move CannotConnectToDatabase from Sherpa/Exceptions

---
+++ src/database/exceptions/CannotConnectToDatabase.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Sherpa\Db\database\exceptions;
+
+use Sherpa\Exceptions\exceptions\SherpaException;
+
+class CannotConnectToDatabase extends SherpaException
+{
+    public function __construct(?Throwable $previous = null)
+    {
+        $message = "Sherpa cannot connect to database.";
+
+        parent::__construct($message, 1201, $previous);
+    }
+}
